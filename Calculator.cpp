6aa94ae4b610836f99c4706445e55153e169a955
Malik Tabish <118992711+ITMalik97@users.noreply.github.com>
2022-11-29 00:04:37 +0500

    calculator using functions

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,8 +4,7 @@ void cal(int , int);  // 1
 void calc(int , int);  // 2
 void calcu(int , int);  // 3
 void calr(float , float);  // 4
-int main()
-{
+int main(){
 	char it;
 	cout<<"enter the operator = ";   // operator
 	cin>>it;
@@ -21,23 +20,19 @@ int main()
 	else if(it == '/')
 	calr(a , b);
 }
-void cal(int a, int b)  // function 1 sum
-{
+void cal(int a, int b){  // function 1 sum
 	int c = a + b;
 	cout<<"the answer is = "<< c <<endl;
 }
-void calc(int a, int b)  // function 2 sub
-{
+void calc(int a, int b){  // function 2 sub
 	int c = a - b;
 	cout<<"the answer is = "<<c<<endl;
 }
-void calcu(int a , int b)  // function 3 mul
-{
+void calcu(int a , int b){  // function 3 mul
 	int c = a * b;
 	cout<<"the answer = "<<c<<endl;
 }
-void calr(float a, float b)  // function 4 div
-{
+void calr(float a, float b){  // function 4 div
 	float c = a / b;
 	cout<<"the answer = "<<c<<endl;
 }
